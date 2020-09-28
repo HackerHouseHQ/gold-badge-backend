ca6221548df5b2c93a0d4c68f41a6f05adf7efc3
Ratnesh <ratnesh.kumar@ripenapps.in>
2020-09-28 12:30:16 +0530

    correction

--- app/Http/Controllers/Api/User/DepartmentController.php
+++ app/Http/Controllers/Api/User/DepartmentController.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Validator;
 
 class DepartmentController extends Controller
 {
-    public function saveDpartmentRequest(Request $request)
+    public function saveDepartmentRequest(Request $request)
     {
         try {
             $validator = Validator::make(
