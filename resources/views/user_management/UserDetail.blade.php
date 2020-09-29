c47fbfcf2f46e64b8b656de997b6d6e585ae6fa4
Ratnesh <ratnesh.kumar@ripenapps.in>
2020-09-29 19:25:51 +0530

    reason api

--- resources/views/user_management/UserDetail.blade.php
+++ resources/views/user_management/UserDetail.blade.php
@@ -206,12 +206,12 @@
         <div class="header-body">
             <div class="row align-items-center py-4">
                 <div class="col-lg-6 col-7">
-                    <h6 class="h2 text-white d-inline-block mb-0">Datatables</h6>
+                    <h6 class="h2 text-white d-inline-block mb-0">User Management</h6>
                     <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
                         <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
                             <li class="breadcrumb-item"><a href="#"><i class="fas fa-home"></i></a></li>
                             <li class="breadcrumb-item"><a href={{route('user')}}>User</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">User Detail</li>
+                            <li class="breadcrumb-item active" aria-current="page">User Review</li>
                             {{-- <li> <button class="btn btn-primary" data-toggle="sweet-alert" data-sweet-alert="confirm">Confim
                   alert</button></li> --}}
 
