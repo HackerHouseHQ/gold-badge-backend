4088003ee5499bf3da1105db81c2cbdb678f55fb
Ratnesh <ratnesh.kumar@ripenapps.in>
2020-10-13 19:07:58 +0530

    inital commit

--- app/Http/Controllers/Api/User/InformationManagementController.php
+++ app/Http/Controllers/Api/User/InformationManagementController.php
@@ -6,6 +6,7 @@ use Exception;
 use App\Notification;
 use App\SendNotification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 
 class InformationManagementController extends Controller
@@ -14,7 +15,11 @@ class InformationManagementController extends Controller
     public function getNotification(Request $request)
     {
         try {
-            $data = SendNotification::all();
+            $data = SendNotification::select(DB::raw('DISTINCT(DATE(created_at)) as date'))->get();
+            foreach ($data as $key => $value) {
+                $notifications = SendNotification::where('created_at', 'LIKE', '%' . date('Y-m-d', strtotime($value->date)) . '%')->get();
+                $value->notifications  = $notifications;
+            }
             return res_success(trans('messages.successFetchList'), array('notificationList' => $data));
         } catch (Exception $e) {
             return res_failed($e->getMessage(), $e->getCode());
