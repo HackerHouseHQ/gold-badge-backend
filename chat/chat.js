e1a2971b0b23b5d1a639ef1ee76adb820de28b8f
Ratnesh <ratnesh.kumar@ripenapps.in>
2020-10-16 19:05:56 +0530

    initalcommit

--- chat/chat.js
+++ chat/chat.js
@@ -26,7 +26,7 @@ module.exports = {
 
                 connection.query(insert, values, (error, rows, fields) => {
                     if (error) {
-                        io.emit("receive_message", {
+                        io.to(room_id).emit("receive_message", {
                             status: false,
                             message: error,
                             result: msgArr
@@ -34,7 +34,7 @@ module.exports = {
                     } else {
                         console.log(msgArr, 'message---------------------**********');
                         console.log(room_id);
-                        io.emit("received", {
+                        io.to(room_id.toString()).emit("received", {
                             status: true,
                             message: 'SUCCESS',
                             result: msgArr
