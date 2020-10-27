72b6117735c0fe63fcfbae4a87356bf7c9f6de82
Ratnesh <ratnesh.kumar@ripenapps.in>
2020-10-27 17:44:39 +0530

    inital commit

--- config/app.php
+++ config/app.php
@@ -177,7 +177,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
-        Pawlox\VideoThumbnail\VideoThumbnailServiceProvider::class
         // Cyberduck\LaravelExcel\ExcelServiceProvider::class,
 
     ],
@@ -232,7 +231,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
-        'VideoThumbnail' => Pawlox\VideoThumbnail\Facade\VideoThumbnail::class
 
     ],
 
