33e1a3a81bb2f63e87e5df5302d209f7192728e6
Ratnesh <ratnesh.kumar@ripenapps.in>
2020-10-27 17:33:39 +0530

    inital commit

--- app/Http/Controllers/Api/User/GalleryController.php
+++ app/Http/Controllers/Api/User/GalleryController.php
@@ -8,8 +8,12 @@ use App\GalleryImages;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use FFMpeg\Driver\FFMpegDriver;
+use FFMpeg\FFMpeg;
+use FFMpeg\FFProbe;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use Pawlox\VideoThumbnail\Facade\VideoThumbnail;
 
 class GalleryController extends Controller
 {
@@ -22,26 +26,37 @@ class GalleryController extends Controller
                 throw new Exception(trans('messages.contactAdmin'), 401);
             }
             $saveFile = $request->saveFile;
+            $media_type = $request->media_type;
+            $videoFile = $request->videoFile;
             if (isset($saveFile) && !empty($saveFile)) {
                 $arr = $saveFile;
                 if (!is_array($arr)) {
                     $arr = json_decode($arr, true);
                 }
                 $i = 1;
+                $j = 0;
                 foreach ($arr as $image) {
                     $file = $image;
                     $extension = $file->getClientOriginalExtension();
                     $filename = time()  . "$i" . "." . $extension;
                     $path = storage_path() . '/app/public/uploads/gallery_image';
                     $file->move($path, $filename);
+                    $videoFile = $videoFile[$j];
+                    $videoExtension = $videoFile->getClientOriginalExtension();
+                    $videoFilename = time()  . "$i" . "." . $videoExtension;
+                    $videoPath = storage_path() . '/app/public/uploads/gallery_video';
+                    $videoFile->move($videoPath, $videoFilename);
                     $insertArray = [
                         'user_id' => Auth::user()->id,
                         'image'  => $filename,
+                        'video' => (!empty($videoFile)) ? $videoFilename : null,
+                        'media_type' => $media_type,
                         'created_at' => CURRENT_DATE,
                         'updated_at' => CURRENT_DATE
                     ];
                     $insertData = GalleryImages::insert($insertArray);
                     $i++;
+                    $j++;
                 }
             }
             if ($insertData) {
@@ -65,6 +80,8 @@ class GalleryController extends Controller
                 $images = GalleryImages::select(
                     'id as image_id',
                     DB::raw("CONCAT('$siteUrl','storage/uploads/gallery_image/', image) as image"),
+                    'media_type',
+                    DB::raw("CONCAT('$siteUrl','storage/uploads/gallery_video/', video) as video"),
                     'created_at'
                 )->where('user_id', Auth::user()->id)->where('created_at', 'LIKE', '%' . date('Y-m-d', strtotime($value->date)) . '%')->get();
                 $value->images  = $images;
--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
+        Pawlox\VideoThumbnail\VideoThumbnailServiceProvider::class
         // Cyberduck\LaravelExcel\ExcelServiceProvider::class,
 
     ],
@@ -231,6 +232,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+        'VideoThumbnail' => Pawlox\VideoThumbnail\Facade\VideoThumbnail::class
 
     ],
 
