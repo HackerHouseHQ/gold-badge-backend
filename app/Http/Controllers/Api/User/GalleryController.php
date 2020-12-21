4124ee6e6db6d04a98e4a09d750247eb60e0651a
Ratnesh <ratnesh.kumar@ripenapps.in>
2020-12-21 14:43:49 +0530

    inital commit

--- app/Http/Controllers/Api/User/GalleryController.php
+++ app/Http/Controllers/Api/User/GalleryController.php
@@ -68,10 +68,9 @@ class GalleryController extends Controller
                 $filename = time()  . "$i" . "." . $extension;
                 $path = storage_path() . '/app/public/uploads/gallery_image';
                 $img = Image::make($file->getRealPath());
-                $img->resize(100, 100, function ($constraint) {
+                $img->resize(50, 50, function ($constraint) {
                     $constraint->aspectRatio();
                 })->save($path . '/' . $filename);
-
                 $file->move($path, $filename);
 
                 if (!empty($videoFile)) {
