f5403292b6c7872e42ba137b8729c2c5f3a7c62c
ratneshkumar84 <ratnesh.kumar@ripenapps.in>
2021-04-02 15:03:16 +0530

    https

--- resources/views/cms/about_us.blade.php
+++ resources/views/cms/about_us.blade.php
@@ -118,7 +118,7 @@
 <script src="{{asset('bootstrap-wysihtml5-master/lib/js/bootstrap.min.js')}}"></script>
 <script src="{{asset('bootstrap-wysihtml5-master/src/bootstrap-wysihtml5.js')}}"></script> --}}
 <script
-    src="http://bootstrap-wysiwyg.github.io/bootstrap3-wysiwyg/components/wysihtml5x/dist/wysihtml5x-toolbar.min.js">
+    src="https://bootstrap-wysiwyg.github.io/bootstrap3-wysiwyg/components/wysihtml5x/dist/wysihtml5x-toolbar.min.js">
 </script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script> -->
--- resources/views/cms/terms.blade.php
+++ resources/views/cms/terms.blade.php
@@ -104,7 +104,7 @@
 @endsection
 @section('script')
 <script
-    src="http://bootstrap-wysiwyg.github.io/bootstrap3-wysiwyg/components/wysihtml5x/dist/wysihtml5x-toolbar.min.js">
+    src="https://bootstrap-wysiwyg.github.io/bootstrap3-wysiwyg/components/wysihtml5x/dist/wysihtml5x-toolbar.min.js">
 </script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script> -->
